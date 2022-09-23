577bbecac66434bfff8be04a221bcac36ad4fd26
yourpallavikkaawdegit config --global user.password your <pallavi.jagtap512@gmail.com>
2022-09-23 11:37:24 +0530

    Added:second commit done by dev2

--- src/com/bridgelabz/basics/Sample1.java
+++ src/com/bridgelabz/basics/Sample1.java
@@ -4,6 +4,7 @@ public class Sample1 {
 
     public static void main(String[] args) {
         System.out.println("sample1 updated by dev-1 => manu");
+        System.out.println("sample2 updated by dev-2 => pallavi");
         System.out.println("main");
     }
 }
