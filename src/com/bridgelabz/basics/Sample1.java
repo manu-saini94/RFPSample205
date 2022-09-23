d4cc3d8f37c7b3f2dfdfb2af4dde257cf00a0a03
manu-saini94 <manu.saini94@gmail.com>
2022-09-23 11:13:32 +0530

    feature 1 first commit by dev-1

--- src/com/bridgelabz/basics/Sample1.java
+++ src/com/bridgelabz/basics/Sample1.java
@@ -3,6 +3,7 @@ package com.bridgelabz.basics;
 public class Sample1 {
 
     public static void main(String[] args) {
+        System.out.println("sample1 updated by dev-1 => manu");
         System.out.println("main");
     }
 }
