0cb631d0254789ddc96a4ff2f155912f48324521
Nagraj Panchal <109675842+NagrajPanchal@users.noreply.github.com>
2022-09-23 18:48:33 +0530

    Update Sample1.java
    Practice git hub 
    push and pull code in Group project
    But i'm getting Error message 
    remote: Permission to manu-saini94/RFPSample205.git denied to NagrajPanchal.

--- src/com/bridgelabz/basics/Sample1.java
+++ src/com/bridgelabz/basics/Sample1.java
@@ -5,6 +5,7 @@ public class Sample1 {
     public static void main(String[] args) {
         System.out.println("sample1 updated by dev-1 => manu");
         System.out.println("sample2 updated by dev-2 => pallavi");
+        System.out.println("sample2 updated by dev-3 => Nagraj");
         System.out.println("main");
     }
 }
